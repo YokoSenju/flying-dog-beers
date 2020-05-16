262abea7a56128c4d4405058fa29bb3b47733771
YokoSenju <65438760+YokoSenju@users.noreply.github.com>
2020-05-16 15:06:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -364,26 +364,7 @@ colors = {
 
 
 ##############TestZone###############
-app.layout = html.Div(children=[
-    html.H1('Header 1'),
-    html.H2('Header 2'),
-    html.Div(
-        children=html.Img(
-            src="https://plot.ly/~chris/1639.png",
-            style={
-                'maxWidth': '100%',
-                'maxHeight': '100%',
-                'marginLeft': 'auto',
-                'marginRight': 'auto'
-            }
-        ),
-        style={
-            'width': 400,
-            'height': 200,
-            'border': 'thin grey solid'
-        }
-    )
-])
+
 #####################################
 
 
